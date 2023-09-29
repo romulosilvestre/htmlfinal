0a2b2e51a05ee0781646e14ef2a8ac4bf8bf6b5e
romulosilvetre <romulo.cesardf@hotmail.com>
2023-09-28 21:12:35 -0300

    produzi o bloom

--- js/texto_abertura.js
+++ js/texto_abertura.js
@@ -1,9 +1,15 @@
 function inicio() {
+    /*let, var e const*/
     const textoOriginal = "Planeta Alien";
+    //DOM -objeto 1 - parágrafo : tag
     const elementoTexto = document.querySelector("p")
+    //DOM - objeto 2 
     const audio = document.querySelector("audio")
+    //DOM - objeto 3
     const btn = document.querySelector("button")
+    //DOM - 5000 milisegundos (5 segundos)
     const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    //DOM - 10000 milisegundo (7 segundos)
     const atrasoDevagar = 7000
 
     setTimeout(function () {
